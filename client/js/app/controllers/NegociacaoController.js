98cc79a7675a33ae25a36cd3e3b67561becde1da
Manuela Bognar <34453910+manuelabognar@users.noreply.github.com>
2019-09-29 14:42:27 -0300

    Add files via upload

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,19 +6,34 @@ class NegociacaoController{
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event){
 
         event.preventDefault();
-        
-        let negociacao = new Negociacao(
-            new Date(this._inputData.value),
+
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        // this._listaNegociacoes.negociacoes.push(this._criaNegociacao());
+        this._limpaFormulario();
+        // this._listaNegociacoes.negociacoes.length = 0;
+
+        console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    _criaNegociacao(){
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value,
         );
+    }
 
-        console.log(negociacao);
+    _limpaFormulario(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
     }
 }
 
